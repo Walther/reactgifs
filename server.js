7f775e29c295bf2a2f80119103e8ea4a0c852bff
Walther <veeti.haapsamo@gmail.com>
2016-01-15 00:03:40 +0200

    Add initial support for POSTing comments

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.post('/api', function (req, res) {
     var body = req.body;
     console.log(body);
 
-    if (body.command="post") {
+    if (body.command==="post") {
         // Creating a new post
+        console.log("Creating new post")
         var id = shortid.generate()
         var content = {
             "id": id,
@@ -35,7 +36,39 @@ app.post('/api', function (req, res) {
             }
             res.send(JSON.stringify(id));
         });
-    } else {
+    }
+
+    else if (body.command==="comment") {
+        // Creating a comment to an existing post
+        console.log("Creating new comment")
+        imageid = body.imageid;
+        imagePath = __dirname + "/data/" + imageid;
+        var commentID = shortid.generate();
+        var content = {
+            "id": commentID,
+            "author": body.author,
+            "text": body.text,
+        }
+
+        fs.readFile(imagePath, 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            
+            var temp = JSON.parse(data);
+
+            temp["comments"].push(content);
+
+            fs.writeFile(imagePath, JSON.stringify(temp), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                res.send(JSON.stringify(commentID));
+            });
+        })
+    }
+
+    else {
         res.sendStatus(400)
     }
 }); 
